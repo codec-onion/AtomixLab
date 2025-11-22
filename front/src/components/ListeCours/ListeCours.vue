9aa1a51ace7966bd0cd43fdd061315061d78b4fc
codec-onion <bonnetmatthieu86@gmail.com>
2025-11-22 10:46:05 +0100

    setting up filters

--- front/src/components/ListeCours/ListeCours.vue
+++ front/src/components/ListeCours/ListeCours.vue
@@ -2,20 +2,50 @@
   <section class="margin_section">
     <HeaderListeCours />
     <div class="cours">
-      <Cours v-for="c in cours" :key="c._id" :infosCours="getInfosCours(c)"/>
+      <Cours v-for="c in coursFiltres" :key="c._id" :infosCours="getInfosCours(c)"/>
     </div>
   </section>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useDonnesStore } from '@/stores/donnes'
+import { useFiltersStore } from '@/stores/filters'
 import HeaderListeCours from './header/HeaderListeCours.vue'
 import Cours from './Cours.vue'
 
 const donneesStore = useDonnesStore()
 const { cours } = storeToRefs(donneesStore)
+
+const filtersStore = useFiltersStore()
+const { sessionFilter, levelFilter, themeFilter } = storeToRefs(filtersStore)
+
+const coursFiltres = computed(() => {
+  // Si les cours ne sont pas encore chargés, retourner un tableau vide
+  if (!cours.value || cours.value.length === 0) {
+    return []
+  }
+
+  return cours.value.filter(c => {
+    // Filtre par session
+    if (sessionFilter.value && sessionFilter.value !== '' && c.session !== sessionFilter.value) {
+      return false
+    }
+
+    // Filtre par niveau scolaire
+    if (levelFilter.value && levelFilter.value !== '' && c.niveauScolaire !== levelFilter.value) {
+      return false
+    }
+
+    // Filtre par thématique
+    if (themeFilter.value && themeFilter.value !== '' && c.thematique !== themeFilter.value) {
+      return false
+    }
+
+    return true
+  })
+})
 
 const getInfosCours = (cours) => {
   const infosCours = {
